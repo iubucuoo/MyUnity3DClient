f4bb51b0731217094f0e061a41ed7f713ada4500
wsj <522742475@qq.com>
2021-09-01 18:13:57 +0800

    BlockPuzzleDemo

---
+++ BlockPuzzleDemo/Assets/Script/UI/DragUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragUI : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+    private Vector2 offsetPos;
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position - offsetPos;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        offsetPos = eventData.position - (Vector2)transform.position;
+    }
+}
---
+++ BlockPuzzleDemo/Assets/Script/UI/UIMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenu : MonoBehaviour
+{
+    public Image img_bg;
+    public Button btn_start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn_start.onClick.AddListener(OnBtnStart);
+    }
+    void OnBtnStart()
+    {
+        Debug.Log("开始游戏");
+        img_bg.gameObject.SetActive(false);
+        btn_start.gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
