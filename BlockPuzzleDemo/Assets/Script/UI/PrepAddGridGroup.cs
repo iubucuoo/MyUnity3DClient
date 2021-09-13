8725e3a4c0de3fa6263b0acd1dac28877f61c8d1
wsj <522742475@qq.com>
2021-09-13 16:53:01 +0800

    setactive
    待放入区域 的显示优化

--- BlockPuzzleDemo/Assets/Script/UI/PrepAddGridGroup.cs
+++ BlockPuzzleDemo/Assets/Script/UI/PrepAddGridGroup.cs
@@ -16,15 +16,15 @@ public class PrepAddGridGroup : MonoBehaviour
         EventTriggerListener.Get(gameObject).onDown = OnPointerDown;
         EventTriggerListener.Get(gameObject).onUp = OnPointerUp;
     }
-     public void SetUse()
+     void SetUse()
     {
         IsUse = true;
         PoolMgr.Recycle(minPrepGroup);//回收
 
         //待放格子区 检测是否可以放置 不能放的变灰 无法使用
-        //如果不能放 执行相应的操作
+        //如果不能放 执行相应的操作(比如游戏结束)
 
-        //判断是否需要刷新
+        //三个格子都用完了，刷新三个待放入的格子
         if (GridGroupMgr.Inst.IsOverPrep())
         {
             GridGroupMgr.Inst.RefreshPrepGridGroup();
@@ -35,23 +35,29 @@ public class PrepAddGridGroup : MonoBehaviour
     {
         minPrepGroup = v;
     }
+    void SetChildActive(bool sw)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(sw);
+        }
+    }
     public void OnPointerUp(GameObject eventData)
     {
         if (IsUse)
         {
             return;
         }
-        DragingGridMgr.Inst.SetDragUp(this);
         Debug.Log("OnPointerUp   " + transform.name);
+        DragingGridMgr.Inst.SetDragUp(this);
         if (GridGroupMgr.Inst.RefreshMainGrid())//如果当前可以放置 刷新主面板显示
         {
             SetUse();//设置当前待放入的group为使用过了
         }
         else
         {
-            //使用失败 跑一个回到原始位置的动画
+            SetChildActive(true);//使用失败 跑一个回到原始位置的动画
         }
-
     }
     public void OnPointerDown(GameObject eventData)
     {
@@ -59,8 +65,9 @@ public class PrepAddGridGroup : MonoBehaviour
         {
             return;
         }
-        DragingGridMgr.Inst.SetDragDown(minPrepGroup);
         Debug.Log("OnPointerDown   " + transform.name);
+        DragingGridMgr.Inst.SetDragDown(minPrepGroup);
+        SetChildActive(false);
     }
     public void Reset()
     {
